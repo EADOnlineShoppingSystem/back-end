46c8117a8a69eb9529a650af410ca28e818f0666
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-11-18 15:01:24 +0530

    api gateway creations

---
+++ api-gateway/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const apiKeyMiddleware =require('./middlewares/apiKeyMiddleware');
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+
+app.use(
+    "/order",
+    createProxyMiddleware({
+        target: "http://localhost:5001",
+        changeOrigin: true,})
+);
+
+app.use(
+    "/auth",
+    createProxyMiddleware({
+        target: "http://localhost:5002",
+        changeOrigin: true,})
+    
+)
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
+    console.log(`API Gateway listening on port ${PORT}`);})
